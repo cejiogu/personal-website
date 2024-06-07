f01a6fb1d7fdb17c2a7c32eb7389f116304cd05f
Chimdi <ce248@cornell.edu>
2024-06-07 17:41:13 -0400

    Implement baseplate for navbar element components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Title from './components/Title/Title';
+import PageTitle from './components/PageTitle/PageTitle';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <PageTitle name="About"/>
     </div>
   );
 }
