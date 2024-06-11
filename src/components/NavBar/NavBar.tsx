1de2733402cf43fb9e6448b0c9f0dff173fd93f0
Chimdi <ce248@cornell.edu>
2024-06-11 15:02:28 -0400

    Implement NavBar component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,1 +1,16 @@
 import React from 'react';
+import PageTitle from './PageTitle/PageTitle';
+// import Title from '../Title/Title';
+import './NavBar.css';
+
+const NavigationBar = () => {
+  return (
+    <div className="NavBar">
+      <PageTitle name="About"/> 
+      <PageTitle name="Experience"/> 
+      <PageTitle name="Contact Me"/>
+    </div>
+  );
+}
+
+export default NavigationBar;
