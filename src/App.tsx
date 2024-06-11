8628f2b690980db365087aa15e96b40623f01c45
Chimdi <ce248@cornell.edu>
2024-06-11 15:01:52 -0400

    Implement baseplate for header, body, and footer divs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import NavigationBar from './components/NavBar/NavBar';
 import Title from './components/Title/Title';
-import PageTitle from './components/PageTitle/PageTitle';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Title />
-      <PageTitle name="About"/>
+    <div className="App" >
+      <div className="Header">
+        <Title />
+        <NavigationBar/>
+      </div>
+      <div className="Body">
+        <p>Hello</p>
+        Placeholder text
+      </div>
+      <div className="Footer">
+      Copyright © 2024 Chimdi Ejiogu. All rights reserved.
+      </div>
     </div>
   );
 }
+
+/*
+Notes:
+- Look into "background" for dynamic background
+
+*/
 
 export default App;
