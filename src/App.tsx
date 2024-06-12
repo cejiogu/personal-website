a56f822548a0967cf2137502b538544d7468bfb7
Chimdi <ce248@cornell.edu>
2024-06-12 06:41:42 -0400

    Add and style dummy text

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App" >
+    <div className="App">
       <div className="Header">
         <Title />
         <NavigationBar/>
       </div>
-      <div className="Body">
+      <div className="TopSection">
         <ProfilePicture />
-        <p>Hello</p>
+        <div className="IntroText" style={{padding: "30px"}}>
+          <h2 className="text">I am a...</h2>
+          <p className="text" style={{maxWidth: "800px", paddingLeft: "25px", fontSize: "22px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
       </div>
+      <div className="MidSection" style={{display: "inline-flex"}}>
+          <div style={{paddingLeft: "25px", maxWidth: "600px"}}>
+            <h3 className="text" style={{display: "flex",justifyContent: "center"}}>Title</h3>
+            <p className="text" style={{fontSize: "22px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
       <div className="Footer">
-      Copyright © 2024 Chimdi Ejiogu. All rights reserved.
+        <p>Copyright © 2024 Chimdi Ejiogu. All rights reserved.</p>
       </div>
     </div>
   );
