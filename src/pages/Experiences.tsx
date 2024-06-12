fbbe5fdf7e8d3f645981e9fadf3206936df4e3ef
Chimdi <ce248@cornell.edu>
2024-06-12 15:11:58 -0400

    Implement baseplate of experiences page

---
+++ src/pages/Experiences.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Experiences.css';
+
+function Experiences() {
+  return (
+    <div>
+      <h2>Past Experiences</h2>
+    </div>
+  );
+}
+
+export default Experiences;
