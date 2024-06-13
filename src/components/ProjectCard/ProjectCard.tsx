17d279d745cb82eaff43a59bdbc248c7bb3597ac
Chimdi <ce248@cornell.edu>
2024-06-13 11:53:41 -0400

    Implement baseplate for project cards

---
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ProjectCard.css';
+
+
+// function ProjectCard(props:{title: string; desc: string; img: string}) {
+function ProjectCard () {
+  return (
+    <div className="outer-card">
+      {/* {props.title}
+      {props.desc}
+      {props.img} */}
+    </div>
+  );
+}
+
+export default ProjectCard;
